ab3244616a940a8c9d48708a1485edf080e88e5c
Akinola Adewole <adewoleakinola24@gmail.com>
2025-05-08 23:56:05 +0100

    About Us component designcompleted

--- src/components/About/AboutUs.jsx
+++ src/components/About/AboutUs.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import Vision from "./Vision";
+import Approch from "./Approch";
 
 const AboutUs = () => {
   return (
-    <div>AboutUs</div>
-  )
-}
+    <div className=" container pt-24">
+      <div>
+        <h1 className=" font-bold text-4xl text-center">
+          About <span className=" text-primary">US</span>
+        </h1>
+      </div>
 
-export default AboutUs
+      <Vision />
+      <Approch />
+    </div>
+  );
+};
+
+export default AboutUs;
